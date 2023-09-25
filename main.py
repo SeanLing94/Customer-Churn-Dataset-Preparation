6aec7117eddcb1306d2facacec19913ba6ac36e7
SeanL <143430094+SeanLing94@users.noreply.github.com>
2023-09-25 22:48:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import pandas as pd
+
+df1 = pd.read_excel ('Customer-Churn-and-Dataset/customer.xlsx')
+df2 = pd.read_excel ('Customer-Churn-and-Dataset/churn.xlsx')
+df3 = pd.read_excel ('Customer-Churn-and-Dataset/ptransaction.xlsx')
 
